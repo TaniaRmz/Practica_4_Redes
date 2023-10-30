00506a594c6b705b5c1f592274df55b0e6f66aa2
TaniaRmz <88344062+TaniaRmz@users.noreply.github.com>
2023-10-30 01:49:32 -0500

    Add files via upload

---
+++ ayuda.py
@@ -0,0 +1,13 @@
+
+print ("Para correr el programa ingresar lo siguiente en terminal: \n"+ "python clientHTTP.py host http_method url user_agent encoding connection\n"+ "donde: \n" + \
+            "\nhost = direción IP o nombre de dominio del servidor HTTP \n" + "Ejemplo: 'www.fcinecias.unam.mx' \n" + \
+            "\nhttp_method = 'HEAD' o GET' \n" + \
+            "\nurl = archivo o recurso solicitado al servidor web \n" + "Ejemplo: '/' para el directorio raíz, o 'imagen.jpg' \n" + \
+            "\nuser_agent = 1, 2, 3 o 4 dependiento del User-Agent que desee emplear de las siguientes opciones: \n"  + \
+                "[1] Firefox\n" + \
+                "[2] Chrome\n" + \
+                "[3] Microsoft Edge\n" + \
+                "[4] Safari\n"+ \
+            "\nencoding = parámetro de la solicitud de la codificación de la respuesta \n" + "Ejemplo: 'gzip', 'deflate' o 'identity' \n" + \
+            "\nconnection = forma del establecimiento de la conexión \n" + "Ejemplo: 'keep-alive' o 'close'")
+
